06c25ab385710a271da5ef33f5fa93e7ef1f89b8
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-12 06:32:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let main = document.geElementById('main');
 main.remove();
-let newHeader=document.createElement('h1');
+let newHeader=document.createElement('h');
 newHeader.id="victory";
 newHeader.innerHTML="NAME is the champion";
 document.body.appendChild(newHeader);
